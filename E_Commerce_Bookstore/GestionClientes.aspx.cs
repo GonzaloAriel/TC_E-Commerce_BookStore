1558f8073eccc1ccb874c1b6a82ee105b8715323
Gonzalo Ariel Acuña <gonzaloarielacuna@gmail.com>
2025-11-27 01:22:57 -0300

    Pagina Gestion de clientes

---
+++ E_Commerce_Bookstore/GestionClientes.aspx.cs
@@ -0,0 +1,127 @@
+﻿using Dominio;
+using Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace E_Commerce_Bookstore
+{
+    public partial class GestionClientes : System.Web.UI.Page
+    {
+        GestionClienteNegocio negocio = new GestionClienteNegocio();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+                CargarGrilla();
+        }
+
+        private void CargarGrilla()
+        {
+            dgvClientes.DataSource = negocio.Listar();
+            dgvClientes.DataBind();
+        }
+
+        protected void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtId.Text = "";
+            txtNombre.Text = "";
+            txtApellido.Text = "";
+            txtDNI.Text = "";
+            txtEmail.Text = "";
+            txtIdUsuario.Text = "";
+            txtTelefono.Text = "";
+            txtDireccion.Text = "";
+            txtCP.Text = "";
+
+            lblMensaje.Text = "";
+        }
+
+        protected void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                negocio.Eliminar(int.Parse(txtId.Text));
+                lblMensaje.Text = "<div class='alert alert-danger'>Cliente eliminado.</div>";
+                CargarGrilla();
+            }
+            catch (Exception ex)
+            {
+                lblMensaje.Text = "<div class='alert alert-danger'>Error: " + ex.Message + "</div>";
+            }
+        }
+
+        protected void btnModificar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ClienteGestionDTO mod = new ClienteGestionDTO
+                {
+                    Id = int.Parse(txtId.Text),
+                    Nombre = txtNombre.Text,
+                    Apellido = txtApellido.Text,
+                    DNI = int.Parse(txtDNI.Text),
+                    Email = txtEmail.Text,
+                    IdUsuario = int.Parse(txtIdUsuario.Text),
+                    Telefono = txtTelefono.Text,
+                    Direccion = txtDireccion.Text,
+                    CP = txtCP.Text
+                };
+
+                negocio.Modificar(mod);
+                lblMensaje.Text = "<div class='alert alert-warning'>Cliente modificado.</div>";
+                CargarGrilla();
+            }
+            catch (Exception ex)
+            {
+                lblMensaje.Text = "<div class='alert alert-danger'>Error: " + ex.Message + "</div>";
+            }
+        }
+
+        protected void btnAgregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ClienteGestionDTO nuevo = new ClienteGestionDTO
+                {
+                    Nombre = txtNombre.Text,
+                    Apellido = txtApellido.Text,
+                    DNI = int.Parse(txtDNI.Text),
+                    Email = txtEmail.Text,
+                    IdUsuario = int.Parse(txtIdUsuario.Text),
+                    Telefono = txtTelefono.Text,
+                    Direccion = txtDireccion.Text,
+                    CP = txtCP.Text
+                };
+
+                negocio.Agregar(nuevo);
+                lblMensaje.Text = "<div class='alert alert-success'>Cliente agregado.</div>";
+                CargarGrilla();
+            }
+            catch (Exception ex)
+            {
+                lblMensaje.Text = "<div class='alert alert-danger'>Error: " + ex.Message + "</div>";
+            }
+        }
+
+        protected void dgvClientes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GridViewRow row = dgvClientes.SelectedRow;
+
+            txtId.Text = row.Cells[0].Text;
+            txtNombre.Text = row.Cells[1].Text;
+            txtApellido.Text = row.Cells[2].Text;
+            txtDNI.Text = row.Cells[3].Text;
+            txtEmail.Text = row.Cells[4].Text;
+            txtTelefono.Text = row.Cells[5].Text;
+
+            ClienteGestionDTO cli = negocio.ObtenerPorId(int.Parse(txtId.Text));
+
+            txtIdUsuario.Text = cli.IdUsuario.ToString();
+            txtDireccion.Text = cli.Direccion;
+            txtCP.Text = cli.CP;
+        }
+    }
+}
